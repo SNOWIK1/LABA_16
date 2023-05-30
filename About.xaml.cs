4a7a7609d5108dbbe85221e53ab19fdcc2759daa
SNOWIK1 <dimais444@mail.ru>
2023-05-30 11:20:43 +0700

    finished

---
+++ About.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LABA_16
+{
+    /// <summary>
+    /// Логика взаимодействия для About.xaml
+    /// </summary>
+    public partial class About : Page
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void go_main(object sender, RoutedEventArgs e)
+        {
+            Menu menu = new Menu();
+            NavigationService.Navigate(menu);
+        }
+    }
+}
---
+++ Menu.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LABA_16
+{
+    /// <summary>
+    /// Логика взаимодействия для Menu.xaml
+    /// </summary>
+    public partial class Menu : Page
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseApp(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void AboutOpen(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new About());
+        }
+
+        private void options(object sender, RoutedEventArgs e)
+        {
+            
+            NavigationService.Navigate(new Options());
+        }
+    }
+}
